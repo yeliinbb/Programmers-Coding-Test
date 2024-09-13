dcc3b9b957e720a78879bf09df8e13ff7cb3378e
YELIIN BOK <156063434+yeliinbb@users.noreply.github.com>
2024-09-13 16:45:22 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -2,9 +2,11 @@ function solution(numbers) {
     var answer = -1;
     // 0부터 9까지의 숫자의 합 - numbers 배열의 원소의 합
     let sum = '';
+    const sumArr = (9*(9+1))/2;
+    // console.log(sumArr);
     const initialValue = 0;
     sum = numbers.reduce((acc, value) => acc + value, initialValue);
     // console.log(sum);
-    answer = 45 - sum;
+    answer = sumArr - sum;
     return answer;
 }
